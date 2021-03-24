6cafb451f4cee53126a068bc1cfe1fba7999e686
Eric <lee.erictae@gmail.com>
2021-03-23 19:39:01 -0500

    init, add indexproducts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <!-- <p><button v-on:click="firstButton">First Button</button></p> -->
+
+    <!-- <div v-for="product in products">
+    {{ product.name }}
+    <hr>
+    </div> -->
   </div>
 </template>
-
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      products: [],
+    };
+  },
+  created: function () {
+    console.log("in created");
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      console.log("in products index");
+      // get data from api, then show that data to the user
+      axios.get("http://localhost:3000/api/products").then((response) => {
+        console.log(response.data);
+        this.products = response.data;
+      });
+    },
   },
 };
 </script>
